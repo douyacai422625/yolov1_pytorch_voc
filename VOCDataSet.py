3124ef63ca8625cbe71c3053fca05b2c122ba1f4
douyacai422625 <1924678362@qq.com>
2021-12-29 18:14:29 +0800

    增加VOC数据迭代

--- VOCDataSet.py
+++ VOCDataSet.py
@@ -1,14 +1,17 @@
+import cv2
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 import torch
-
+import random
 import numpy as np
 
 class VOCDataset(Dataset):
-    def __init__(self,label_txt,img_size = 448,grid_size = 7,num_bboxes = 2,num_classes = 20):
+    def __init__(self,label_txt,train_label,img_size = 448,grid_size = 7,num_bboxes = 2,num_classes = 20):
         self.S = grid_size
         self.B = num_bboxes
         self.C = num_classes
+
+        self.train_label = train_label
 
         mean_rgb = [122.67891434, 116.66876762, 104.00698793]
         self.mean = np.array(mean_rgb, dtype=np.float32)
@@ -18,9 +21,9 @@ class VOCDataset(Dataset):
         with open(label_txt) as f:
             lines = f.readlines()
 
-        box, label = [], []
         self.paths, self.boxes, self.labels = [], [], []
         for line in lines:
+            box, label = [], []
             splitted = line.strip().split(' ')
             path = splitted[0]
             self.paths.append(path)
@@ -35,24 +38,45 @@ class VOCDataset(Dataset):
                 label.append(c)
             self.boxes.append(torch.Tensor(box))
             self.labels.append(torch.LongTensor(label))
-            a = 1
         self.num_samples = len(self.paths)
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        img = cv2.imread(path)
+        boxes = self.boxes[idx].clone() # [n, 4]
+        labels = self.labels[idx].clone() # [n,]
+        for bbox in boxes:
+            x1,y1,x2,y2 = bbox
+            x1 = int(x1.numpy())
+            y1 = int(y1.numpy())
+            x2 = int(x2.numpy())
+            y2 = int(y2.numpy())
+        if self.train_label:
+            new_img,boxes = self.random_flip(img,boxes)
+            for bbox in boxes:
+                x1, y1, x2, y2 = bbox
+                x1 = int(x1.numpy())
+                y1 = int(y1.numpy())
+                x2 = int(x2.numpy())
+                y2 = int(y2.numpy())
+                try:
+                    cv2.rectangle(new_img, pt1= (x1, y1), pt2 = (x2, y2), color=(0, 0, 255), thickness=2)
+                except:
+                    print(idx)
 
+    def random_flip(self, img, boxes):
+        # if random.random() < 0.5:
+        #     return img, boxes
 
-            # fname = splitted[0]
-            # path = os.path.join(image_dir, fname)
-            # self.paths.append(path)
-            #
-            # num_boxes = (len(splitted) - 1) // 5
-            # box, label = [], []
-            # for i in range(num_boxes):
-            #     x1 = float(splitted[5*i + 1])
-            #     y1 = float(splitted[5*i + 2])
-            #     x2 = float(splitted[5*i + 3])
-            #     y2 = float(splitted[5*i + 4])
-            #     c  =   int(splitted[5*i + 5])
-            #     box.append([x1, y1, x2, y2])
-            #     label.append(c)
-            # self.boxes.append(torch.Tensor(box))
-            # self.labels.append(torch.LongTensor(label))
+        h, w, _ = img.shape
 
+        img = np.fliplr(img)
+
+        x1, x2 = boxes[:, 0], boxes[:, 2]
+        x1_new = w - x2
+        x2_new = w - x1
+        boxes[:, 0], boxes[:, 2] = x1_new, x2_new
+
+        return img, boxes
+
+    def __len__(self):
+        return self.num_samples
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import cv2
 import os
 import torch
-
+from torch.utils.data import DataLoader
 from Gen_train_data import Gen_VOC_data
 from model import Model
 from Loss import loss
 from VOCDataSet import VOCDataset
+import tqdm
+
+### 参数
+# 超参数
+init_lr = 0.001
+momentum = 0.9
+weight_decay = 5.0e-4
+
+batch_size = 1
 
 label_file = 'label_file.txt'
 # voc_path = '/media/li/b806bc78-4cbd-4e31-ba5c-e0212d292e732/data/VOC/VOC2012'
 # VOC_data = Gen_VOC_data()
 # VOC_data.data_process(voc_path,label_file)
-train_dataset = VOCDataset(label_file)
-
+train_dataset = VOCDataset(label_file,True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
 use_gpu = torch.cuda.is_available()
-assert use_gpu, 'Current implementation does not support CPU mode. Enable CUDA.'
-print('CUDA current_device: {}'.format(torch.cuda.current_device()))
-print('CUDA device_count: {}'.format(torch.cuda.device_count()))
 
 # 重写yolov1 模型
 yolov1_model = Model()
-
-# Training hyper parameters.
-init_lr = 0.001
-base_lr = 0.01
-momentum = 0.9
-weight_decay = 5.0e-4
-num_epochs = 135
-batch_size = 64
 
 # 调整学习率
 criterion = loss()
 optimizer = torch.optim.SGD(yolov1_model.parameters(), lr=init_lr, momentum=momentum, weight_decay=weight_decay)
 
-#
+
+
+
+# 训练
+for imgs, targets in train_loader:
+    print('循环')
+    a = 1
